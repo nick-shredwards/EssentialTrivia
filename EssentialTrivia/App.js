49cd566f6a040b98d091f575c071c63972ea1492
A Edwards <Nick@As-MacBook-Pro-2.local>
2020-05-07 20:11:46 -0600

    First app commit

--- EssentialTrivia/App.js
+++ EssentialTrivia/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+   
+export default class Homepage extends Component {
+    state = {
+        names: [
+            { id: 0, name: 'Categories', color: 'skyblue',},
+            { id: 1, name: 'QuickMatch', color: 'skyblue',},
+            { id: 2, name: 'High Scores', color: 'skyblue',}
+        ]
+    }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    render() {
+        return (
+            <View style={{
+              flex: 1,
+              justifyContent: 'space-around',
+              width: '100%',
+              alignItems: 'center'
+              }}>
+              {
+                  this.state.names.map((item, index) => (
+                    <Button
+                      key = {item.id}
+                      color = {item.color}
+                      title = {item.name} 
+                    />
+                  ))
+              }
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
