69ed18f116d23c46e37c9e7e7ae24906d6813c14
A Edwards <Nick@As-MacBook-Pro-2.local>
2020-05-11 17:52:35 -0600

    Page Switch without psql

--- EssentialTrivia/App.js
+++ EssentialTrivia/App.js
@@ -1,61 +1,68 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { Text, View, Button, AppRegistry, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+function HomePage({navigation}) {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'space-around',
+      width: '100%',
+      alignItems: 'center'
+      }}>
+      <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>Essential Trivia</Text>
+      <Button
+        title = "Quickmatch"
+        onPress = {() => navigation.navigate('QuestionPage')}
+      />
+      <Button
+        title = "High Scores"
+      />
+      <Button 
+        title = "Categories"
+      />
+    </View>
+  );
+}
+
+function QuestionPage({navigation}) {
+  return(
+    <View style={{
+      flex: 1,
+      width: '100%',
+      alignItems: 'center'
+      }}>
+      <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>Question</Text>
+      <Button
+        title = "a"
+        onPress = {() => navigation.navigate('QuestionPage')}
+      />
+      <Button
+        title = "b"
+      />
+      <Button 
+        title = "c"
+      />
+        <Button 
+        title = "d"
+      />
+
+    </View>
+  );
+}
+
 const Stack = createStackNavigator();
 
-function Etstack(){
-  return(
+function App() {
+  return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-        name = "Home"
-        component = {Home}
-        options
-        />
-
-        <Stack.Screen name = "QuestionPage" component = {Question}/>
+      <Stack.Navigator initialRoutName = "HomePage">
+        <Stack.Screen name="HomePage" component={HomePage} />
+        <Stack.Screen name="QuestionPage" component = {QuestionPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default class Homepage extends Component {
-    state = {
-        names: [
-            { id: 0, name: 'Categories', color: 'skyblue',},
-            { id: 1, name: 'QuickMatch', color: 'skyblue',},
-            { id: 2, name: 'High Scores', color: 'skyblue',}
-        ]
-    }
-
-   handlePress = () => {
-    navigation.navigate('')
-  }
-
-
-    render() {
-        return (
-            <View style={{
-              flex: 1,
-              justifyContent: 'space-around',
-              width: '100%',
-              alignItems: 'center'
-              }}>
-              <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>Essential Trivia</Text>
-              {
-                  this.state.names.map((item, index) => (
-                    <Button
-                      key = {item.id}
-                      color = {item.color}
-                      onPress = {() => this.handlePress()}
-                      title = {item.name} 
-                    />
-                  ))
-              }
-            </View>
-        );
-    }
-}
+export default App;
