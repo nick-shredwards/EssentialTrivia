81ec2127e1703bd8204f11ff3bccb515b23b9887
A Edwards <Nick@As-MacBook-Pro-2.local>
2020-05-11 20:39:56 -0600

    Final Baseline

--- EssentialTrivia/App.js
+++ EssentialTrivia/App.js
@@ -21,6 +21,7 @@ function HomePage({navigation}) {
       />
       <Button 
         title = "Categories"
+        onPress = {() => navigation.navigate('Categories')}
       />
     </View>
   );
@@ -31,23 +32,82 @@ function QuestionPage({navigation}) {
     <View style={{
       flex: 1,
       width: '100%',
+      justifyContent: 'space-around',
       alignItems: 'center'
       }}>
-      <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>Question</Text>
+      <Text style = {{ fontSize: 20, fontWeight: 'bold'}}>What is Nick's Favorite color?</Text>
       <Button
-        title = "a"
+        title = "Purple"
+        onPress = {() => navigation.navigate('Winner')}
+      />
+      <Button
+        title = "Blue"
+        onPress = {() => navigation.navigate('Loser')}
+      />
+      <Button 
+        title = "Yellow"
+        onPress = {() => navigation.navigate('Loser')}
+      />
+        <Button 
+        title = "Red"
+        onPress = {() => navigation.navigate('Loser')}
+      />
+
+    </View>
+  );
+}
+
+function Categories({navigation}) {
+  return(
+    <View style={{
+      flex: 1,
+      width: '100%',
+      alignItems: 'center'
+      }}>
+      <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>Categories</Text>
+      <Button
+        title = "Nick's Favorite color"
         onPress = {() => navigation.navigate('QuestionPage')}
       />
       <Button
-        title = "b"
-      />
-      <Button 
-        title = "c"
-      />
-        <Button 
-        title = "d"
+        title = "Black Holes"
       />
 
+    </View>
+  );
+}
+
+
+function Loser({navigation}) {
+  return(
+    <View style={{
+      flex: 1,
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center'
+      }}>
+      <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>You Lose!</Text>
+      <Button 
+      title = "Back to Home"
+      onPress = {() => navigation.navigate('HomePage')}
+      />
+    </View>
+  );
+}
+
+function Winner({navigation}) {
+  return(
+    <View style={{
+      flex: 1,
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center'
+      }}>
+      <Text style = {{ fontSize: 40, fontWeight: 'bold'}}>You Win!</Text>
+      <Button 
+      title = "Back to Home"
+      onPress = {() => navigation.navigate('HomePage')}
+      />
     </View>
   );
 }
@@ -60,6 +120,9 @@ function App() {
       <Stack.Navigator initialRoutName = "HomePage">
         <Stack.Screen name="HomePage" component={HomePage} />
         <Stack.Screen name="QuestionPage" component = {QuestionPage} />
+        <Stack.Screen name= "Categories" component = {Categories} />
+        <Stack.Screen name = "Loser" component = {Loser} />
+        <Stack.Screen name = "Winner" component = {Winner} />
       </Stack.Navigator>
     </NavigationContainer>
   );
